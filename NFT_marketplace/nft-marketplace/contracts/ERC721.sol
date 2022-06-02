47672152bf172a777091eb73b468fe39f81cfe03
pajunenpyry <pajunenpyry@gmail.com>
2022-06-02 21:18:58 +0200

    starting dev ERC721

---
+++ NFT_marketplace/nft-marketplace/contracts/ERC721.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract ERC721 {
+
+    // remember 3 indexed keyword are MAX per event! 
+    // save gas as much as possible-
+    event Transfer(
+        address indexed _from, 
+        address indexed _to, 
+        uint indexed tokenId);
+
+    // mapping for token id to the owner
+    mapping(uint => address) private _tokenOwner;
+
+    // Mapping from owner to number ofowned tokens
+    mapping(address => uint) private _ownedTokensCount;
+
+    function _exists(uint toeknID) internal view returns(bool) {
+        // setting the address pf nft owner to check the mapping
+        // of the address from tokenOwner at the tokenId
+        address owner = _tokenOwner[toeknID];
+        // return truthiness that address is not zero
+        return owner != address(0);
+    }
+
+    function _mint(address _to, uint _tokenId) internal {
+        require(_to != address(0), "ERC721: minting to the zero address");
+        // Check if the token is already minted
+        require(!_exists(_tokenId), "ERC721: token already minted");
+        // this will give an address to token
+        _tokenOwner[_tokenId] = _to;
+        // add 1 for user owned tokens
+        _ownedTokensCount[_to] +=1;
+        emit Transfer(address(0), _to, _tokenId);
+
+    }
+
+}
